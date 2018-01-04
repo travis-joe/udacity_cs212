a5bbc2842a68f7de2cfed345535e157393daa846
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-01-04 18:26:01 +0800

    lesson3.10

---
+++ Lesson3/reg.py
@@ -0,0 +1,43 @@
+def search(pattern, text):
+    if pattern.startswith('^'):
+        return match(pattern[1:], text)
+    else:
+        return match('.*'+pattern, text)
+
+
+def match(pattern, text):
+    """
+    Return True if pattern appears at the start of text
+
+    For this quiz, please fill in the return values for:
+        1) if pattern == '':
+        2) elif pattern == '$':
+    """
+
+    if pattern == '':
+        return True
+    elif pattern == '$':
+        return text == ''
+    elif len(pattern) > 1 and pattern[1] in '*?':
+        p, op, pat = pattern[0], pattern[1], pattern[2:]
+        if op == '*':
+            return match_star(p, pat, text)
+        elif op == '?':
+            if match1(p, text) and match(pat, text[1:]):
+                return True
+            else:
+                return match(pat, text)
+    else:
+        return match1(pattern[0], text) and match(pattern[1:], text[1:])
+
+
+def match1(p, text):
+    if not text:
+        return False
+    return p == '.' or p == text[0]  # 任意字符或者匹配的字符
+
+
+def match_star(p, pattern, text):
+    return match(pattern, text) or (match1(p, text) and match_star(p, pattern, text[1:]))
+
+
